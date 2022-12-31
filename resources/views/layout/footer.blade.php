43ea6dde49511285dfcb521ff5ecf4c15049daa3
frans <71000758+greyblood@users.noreply.github.com>
2022-12-31 17:06:44 +0700

    Add files via upload
    Navbar and Footer update

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,11 +1,11 @@
-<div class="container-fluid d-flex flex-column" style="background-color:#bed2be; text-align:center;">
-    <div class="" style="padding-left: 9.7%">
-        <div class="" style="color: green; font-family: Comfortaa; font-size:30px; text-align:start"  >Tripify</div>
+<div class="container-fluid d-flex flex-column" style="background-color:#EFF5EE; text-align:center;">
+    <div class="pt-4" style="padding-left: 9.6%;">
+        <div class="" style="color: #00A651; font-family: Comfortaa; font-size:30px; text-align:start"  >Tripify</div>
     </div>
     <div class="d-flex flex-row" style="padding-left:8.7%;">
         <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Home</a>
         <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Tour</a>
-        <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Request Tour</a>
+        <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Request Trip</a>
         <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Guide</a>
     </div>
     <div class="">
@@ -14,14 +14,14 @@
             <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Register</a>
             <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Login</a>
         </div>
-        <div style="text-align: end; padding-right:9%">
-            <a href="" style="text-decoration: none">
+        <div style="text-align: end; padding-right:9%; font-size:25px;">
+            <a href="https://www.facebook.com/" style="text-decoration: none; color:grey" class="me-2">
                 <i class="bi bi-facebook"></i>
             </a>
-            <a href="" style="text-decoration: none">
-                <i class="bi bi-instagram"></i>
+            <a href="https://www.instagram.com/" style="text-decoration: none; color:grey" class="me-2">
+                <i class="bi bi-instagram" ></i>
             </a>
-            <a href="" style="text-decoration: none">
+            <a href="https://www.pinterest.com/" style="text-decoration: none; color:grey">
                 <i class="bi bi-pinterest"></i>
             </a>
         </div>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,23 +1,19 @@
 <div class="navbar navbar-expand-lg bg-body-tertiary d-flex flex-row" style="align-items: center; justify-content:space-around">
     <div>
-      <div class="navbar-brand" style="color: green; font-family: Comfortaa; font-size:30px"  >Tripify</div>
+      <div class="navbar-brand" style="color: #00A651; font-family: Comfortaa; font-size:30px"  >Tripify</div>
     </div>
-
-    {{-- <div class="d-flex flex-row " id="navbarNavAltMarkup" style="align-items: center; justify-content:space-evenly" > --}}
-        <div class="navbar-nav" style="align-items:center; justify-content:space-evenly">
-            <a class="nav-link m-2" href="#">Home</a>
-            <a class="nav-link m-2" href="#">Tour</a>
-            <a class="nav-link m-2" href="#">Request Tour</a>
-            <a class="nav-link m-2" href="#">Guide</a>
-            <a class="nav-link" href="#">About Us</a>
-        </div>
-    {{-- </div> --}}
-
-    <div>
-        <a href="" style="color: black" class="m-3"> 
+    <div class="navbar-nav" style="align-items:center; justify-content:space-evenly">
+        <a class="nav-link m-2" href="#">Home</a>
+        <a class="nav-link m-2" href="#">Tour</a>
+        <a class="nav-link m-2" href="#">Request Trip</a>
+        <a class="nav-link m-2" href="#">Guide</a>
+        <a class="nav-link" href="#">About Us</a>
+    </div>
+    <div style="font-size:25px">
+        <a href="" style="color: black;" class="m-3"> 
             <i class="bi bi-cart3" ></i>
         </a>
-        <a href="" style="color: black"> 
+        <a href="" style="color: black;"> 
             <i class="bi bi-person-circle"></i>
         </a>
     </div>
