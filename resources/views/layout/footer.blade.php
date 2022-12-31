0f484862f999f6c3ec137ab890b04111beb4d418
frans <71000758+greyblood@users.noreply.github.com>
2022-12-31 16:23:45 +0700

    Add files via upload
    Adding Template, Header and Footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,31 @@
+<div class="container-fluid d-flex flex-column" style="background-color:#bed2be; text-align:center;">
+    <div class="" style="padding-left: 9.7%">
+        <div class="" style="color: green; font-family: Comfortaa; font-size:30px; text-align:start"  >Tripify</div>
+    </div>
+    <div class="d-flex flex-row" style="padding-left:8.7%;">
+        <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Home</a>
+        <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Tour</a>
+        <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Request Tour</a>
+        <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Guide</a>
+    </div>
+    <div class="">
+        <div class="d-flex flex-row" style="padding-left:8.7%;" >
+            <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">About Us</a>
+            <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Register</a>
+            <a class="mb-1 m-4" style="color:black; text-decoration:none;" href="#">Login</a>
+        </div>
+        <div style="text-align: end; padding-right:9%">
+            <a href="" style="text-decoration: none">
+                <i class="bi bi-facebook"></i>
+            </a>
+            <a href="" style="text-decoration: none">
+                <i class="bi bi-instagram"></i>
+            </a>
+            <a href="" style="text-decoration: none">
+                <i class="bi bi-pinterest"></i>
+            </a>
+        </div>
+    </div>
+      <hr>
+    <p class="" style="font-size:20px; text-align:end; padding-right:9%"> &#169; 2022</p>
+</div>
