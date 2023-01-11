23e764c7f2c77d1e5466d17d6c78bca70cfbedb8
billych43 <102530883+billych43@users.noreply.github.com>
2023-01-12 00:44:26 +0700

    Add files via upload

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Province;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class DestinationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function showProvince(Request $request)
+    {
+        $province = Province::all();
+
+        return view('addDestination', compact('province'));
+    }
+    public function store(Request $request)
+    {
+        $rules = [
+            'place_name' => 'required|unique:places,place_name',
+            'place_image' => 'required',
+            'province_id' => 'required'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return back()->withErrors($validator);
+        }
+        $place_image = [];
+        foreach ($request->file('place_image') as $key => $image) {
+            $images = $image->hashName();
+            Storage::putFileAs('public/images', $image,$images);
+            
+            if ($images){
+                array_push($place_image, $images);
+            }
+        }
+
+
+        $i = 0;
+        foreach ($request->place_name as $place) {
+            DB::table('places')->insert([
+                'province_id' => $request->province_id,
+                'place_name' => $place,
+                'place_image' => $place_image[$i]
+            ]);
+            $i++;
+        }
+        return redirect('/');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
