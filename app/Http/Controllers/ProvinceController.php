c0aa5ca8ff43fb6f17f4c90985f14f1bb22ec2ac
Fransiscus Chandra <fransiscuschandra.28@gmail.com>
2023-01-25 14:20:12 +0700

    Add Provinces

--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Province;
 use App\Models\TourPlace;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ProvinceController extends Controller
 {
@@ -12,5 +14,26 @@ class ProvinceController extends Controller
         $provinces = Province::all();
 
         return view('addTour', compact('provinces'));
+    }
+    public function showProvinces(){
+        $provinces = Province::all();
+
+        return view('addProvince', compact('provinces'));
+    }
+
+    public function store(Request $request){
+        $rules = [
+            'prov' => 'required|unique:provinces,province_name'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return back()->withErrors($validator);
+        } 
+
+        DB::table('provinces')->insert([
+            "province_name" =>  $request->prov
+        ]);
+        return redirect('/');
     }
 }
